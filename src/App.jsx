71f2639306ade1c61f7232b6e5bdf957fda7d62a
JhZair <johnzairoros@gmail.com>
2025-05-13 16:57:14 -0500

    cambio en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,6 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Simulacros from "./pages/Simulacros";
 import Examen from "./pages/Examen";
-import { useAuth } from "./context/AuthContext";
-
-// Componente para proteger rutas que requieren autenticación
-const PrivateRoute = ({ children }) => {
-  const { currentUser } = useAuth();
-  return currentUser ? children : <Navigate to="/login" />;
-};
-
-// Componente para redirigir usuarios autenticados lejos de las páginas de autenticación
-const PublicRoute = ({ children }) => {
-  const { currentUser } = useAuth();
-  return !currentUser ? children : <Navigate to="/" />;
-};
 
 export default function App() {
   return (
@@ -27,33 +14,25 @@ export default function App() {
         <Route 
           path="/login" 
           element={
-            <PublicRoute>
               <Login />
-            </PublicRoute>
           } 
         />
         <Route 
           path="/signup" 
           element={
-            <PublicRoute>
               <Signup />
-            </PublicRoute>
           } 
         />
         <Route 
           path="/simulacros" 
           element={
-            <PrivateRoute>
               <Simulacros />
-            </PrivateRoute>
           } 
         />
         <Route 
           path="/simulacros/:cursoSlug" 
           element={
-            <PrivateRoute>
               <Examen />
-            </PrivateRoute>
           } 
         />
       </Routes>
